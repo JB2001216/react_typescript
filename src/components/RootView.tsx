82d995e6f7c1a4ab96ab40e22bdea4db7e2dab70
JB2001216 <zhangxing2002611@gmail.com>
2019-08-29 18:58:37 +0200

    No EntriesList but its reference in RootView

--- src/components/RootView.tsx
+++ src/components/RootView.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 
 import { IToDoEntry } from "../interfaces";
 
+// @TODO dichiara un componente EntriesList, che ritorna un Hello World! nella render
 import { EntriesList } from "./EntriesList";
 
 interface IState {
