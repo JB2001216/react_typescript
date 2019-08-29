8c9a4c7851ac2c79bdda01510642e363f96de52a
JB2001216 <zhangxing2002611@gmail.com>
2019-08-29 19:18:05 +0200

    First start

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,2 @@
-import * as ReactDOM from "react-dom";
-
 // tslint:disable-next-line no-import-side-effect
 import "./index.scss";
-
-// @TODO importa App
-
-// JSX syntax is fully supported
-ReactDOM.render(<App />, document.getElementById("app"));
